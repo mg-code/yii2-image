d076acbdeb30e4864c0b1420fd174ea303bb72c3
Maris Graudins <maris@mg-interactive.lv>
2016-12-09 19:15:59 +0200

    database table and models.

---
+++ models/Image.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace mgcode\image\models;
+
+use mgcode\helpers\ActiveRecordHelperTrait;
+
+/**
+ * This is the model class for table "image".
+ */
+class Image extends AbstractImage
+{
+    use ActiveRecordHelperTrait;
+}
